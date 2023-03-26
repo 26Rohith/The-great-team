fb886fa32112287e05ebb4a1b61820de850f09d6
26Rohith <110718952+26Rohith@users.noreply.github.com>
2023-03-26 17:47:48 +0530

    Update aboutInterview.py

--- aboutInterview.py
+++ aboutInterview.py
@@ -1,1 +1,6 @@
-print("This is about interview page")
+
+def about():
+    with open("content.txt",'r') as con:
+        content=con.read()
+    with open("All about your interview.pdf",'w') as d:
+    	d.write(content)
