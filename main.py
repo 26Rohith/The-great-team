6934205373f8eb0dfc421685459c5c89929b921d
26Rohith <110718952+26Rohith@users.noreply.github.com>
2023-03-26 13:38:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("Hello World")
+import aboutInterview
+print("Welcome to interview buddy")
+print("How can we assist you?")
+while True:
+  print("1.About Interviews\n2.Make a resume\n3.Write a cover letetr\n4.Important questions\n5.Tips and Suggetsions\n6.Body Language\n7.End the application")
+  ip1=int(input("Enter your option: "))
+  match ip1:
+    case 1:
+      
